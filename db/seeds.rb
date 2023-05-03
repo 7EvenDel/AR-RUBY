25b63da63081c9f3a82645cabfc26f7e73dae15d
7EvenDel <artur.r.martunovskyi@gmail.com>
2023-05-03 21:01:29 +0300

    commit

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+# movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Movie.create(
+  [
+    { name: "Шахрайки " , year: 2019 },
+    { name: "Джуманджі: Наступний рівень" , year: 2019 },
+    { name: "Лють " , year: 2014 },
+    { name: "Книга джунглів" , year: 2016 },
+    { name: "Бладшот ", year: 2020 },
+    { name: "Погані хлопці назавжди", year: 2020 },
+    { name: "Мисливець на монстрів ", year: 2020 },
+    { name: "Кладовище домашніх тварин", year: 2019 },
+    { name: "Гонка ", year: 2013 },
+    { name: "Робот Чаппі", year: 2015 },
+    { name: "Інший світ 2: Еволюція", year: 2005 },
+    { name: "Людина, яка змінила все." , year: 2011 },
+    { name: "Паразитиn" , year: 2019 },
+    { name: "Субмарина", year: 2010 },
+  ]
+)
